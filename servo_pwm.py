671012af910da19819eae11523a825e3bcb7eae7
MaYaSeVeN <mayaseven@hotmail.com>
2013-12-05 13:47:09 +0700

    first commotted

---
+++ servo_pwm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+hz = 50
+
+degree0 = 0.0005
+degree90 = 0.0015
+degree180 = 0.0025
+
+degree = [degree0, degree90 , degree0, degree180]
+def pwm(a,b):
+	for i in range(hz):
+		GPIO.output(7,1)
+		time.sleep(a)	
+		GPIO.output(7,0)
+		time.sleep(b)
+
+if __name__ == "__main__":
+	while 1:
+        	for a in degree:
+			b = (1 - (a * hz)) / hz
+			print a, b
+			pwm(a,b)
+</pre></body></html>
